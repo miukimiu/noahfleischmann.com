8864a60eff504cec1a1fd720d7870423bffa3cf6
Noah Fleischmann <noahfleischmann21@gmail.com>
2020-08-30 21:32:08 +0200

    View hierarchy change for ProjectCard

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -6,7 +6,7 @@ const ProjectCard = ({ title, description, link, background }) => {
     <Box align="center" borderRadius="lg" p={6} background={background} color="gray.50">
       <Link mb={4} href={link} isExternal _hover={{ textDecoration: "none", boxShadow: "xl" }}>
         <Stack>
-          <Heading as="h4" size="md" fontWeight="bold" mb={4} letterSpacing="tighter">
+          <Heading as="h3" size="md" fontWeight="bold" mb={4} letterSpacing="tighter">
             {title}
           </Heading>
           <Text lineHeight="1.3">{description}</Text>
