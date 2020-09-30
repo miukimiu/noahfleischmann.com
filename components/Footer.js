a8c04000b6225e98b985d79792050c110079217a
Noah Fleischmann <noahfleischmann21@gmail.com>
2020-09-30 22:18:59 +0200

    Finish implementation of RSS feed

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, IconButton, Text, Stack, Flex, Box } from "@chakra-ui/core";
+import { WEBSITE_URL, HANDLE_TWITTER, HANDLE_GITHUB } from "utils/configuration";
 
 import useColors from "hooks/useColors";
 
@@ -21,14 +22,17 @@ const Footer = () => {
         color={tertiaryTextColor}
       >
         <Stack isInline spacing={2} mb={[2, 0]}>
-          <Link href="https://twitter.com/noahfle" title="Twitter" isExternal>
+          <Link href={`https://twitter.com/${HANDLE_TWITTER}`} title="Twitter" isExternal>
             <IconButton aria-label="Twitter" icon="twitter" size="lg" variant="link" color={tertiaryTextColor} />
           </Link>
-          <Link href="https://github.com/fnoah" title="GitHub" isExternal>
+          <Link href={`https://github.com/${HANDLE_GITHUB}`} title="GitHub" isExternal>
             <IconButton aria-label="GitHub" icon="github" size="lg" variant="link" color={tertiaryTextColor} />
           </Link>
           <Link href="mailto:nfleischm@gmail.com" title="Email" isExternal>
             <IconButton aria-label="Email" icon="mail" size="lg" variant="link" color={tertiaryTextColor} />
+          </Link>
+          <Link href={`${WEBSITE_URL}/feed.xml`} title="RSS Feed" isExternal>
+            <IconButton aria-label="RSS Feed" icon="rss" size="lg" variant="link" color={tertiaryTextColor} />
           </Link>
         </Stack>
         <Text>{new Date().getFullYear()} Noah Fleischmann</Text>
--- utils/configuration.js
+++ utils/configuration.js
@@ -1,6 +1,6 @@
 const WEBSITE_URL = "https://noahfleischmann.com";
-const HANDLE_TWITTER = "@noahfle";
-const HANDLE_GITHUB = "@fnoah";
+const HANDLE_TWITTER = "noahfle";
+const HANDLE_GITHUB = "fnoah";
 const AUTHOR = "Noah fleischmann";
 
 module.exports = { WEBSITE_URL, HANDLE_TWITTER, HANDLE_GITHUB, AUTHOR };
